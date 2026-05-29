f273807099e257ba3b2f3023b15f8ef7510d7e2b
test <test@gmail.com>
2026-05-29 16:48:24 +0800

    啟動參數調整

--- start.bat
+++ start.bat
@@ -7,5 +7,5 @@ if not exist .venv\Scripts\activate.bat (
     exit /b 1
 )
 call .venv\Scripts\activate.bat
-python count_inout_head.py --line-y 350 --lite --api-url https://doorway-counter-api.rd-02f.workers.dev/ingest/live/event --zone neihu-hq%*
+python count_inout_head.py --conf-thres 0.15 --auto-exposure 0.25 --exposure -4.5 --clahe --line-y 255 --source 1 --lite --api-url https://doorway-counter-api.rd-02f.workers.dev/ingest/live/event --zone hualien-d %*
 pause
